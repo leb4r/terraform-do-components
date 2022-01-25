84d8ad53aa2f21d5973460647d224c12a3d02c62
leb4r <88212774+leb4r@users.noreply.github.com>
2022-01-25 00:15:36 -0600

    project: update outputs and variables

--- modules/project/outputs.tf
+++ modules/project/outputs.tf
@@ -1,3 +1,8 @@
+output "created_at" {
+  description = "The date and time when the project was created (ISO8601)"
+  value       = digitalocean_project.this.created_at
+}
+
 output "id" {
   description = "ID of the project created"
   value       = digitalocean_project.this.id
@@ -6,4 +11,19 @@ output "id" {
 output "name" {
   description = "The Name of the project created"
   value       = digitalocean_project.this.name
+}
+
+output "owner_id" {
+  description = "The ID of the project owner"
+  value       = digitalocean_project.this.owner_id
+}
+
+output "owner_uuid" {
+  description = "The unique universal identifier of the project owner"
+  value       = digitalocean_project.this.owner_uuid
+}
+
+output "updated_at" {
+  description = "The date and time when the project was last updated (ISO8601)"
+  value       = digitalocean_project.this.updated_at
 }
