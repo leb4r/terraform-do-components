c3cd9aacd36075988e87ee041893e2a55b0f4759
leb4r <88212774+leb4r@users.noreply.github.com>
2022-01-25 00:06:52 -0600

    vpc: add more outputs

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,4 +1,19 @@
-output "vpc_id" {
-  description = "ID of the VPC that was created"
+output "created_at" {
+  description = "The date and time of when the VPC was created"
+  value       = digitalocean_vpc.this.created_at
+}
+
+output "default" {
+  description = "A boolean indicating whether or not the VPC is the default for the region"
+  value       = digitalocean_vpc.this.default
+}
+
+output "id" {
+  description = "The unique identifier for the VPC"
   value       = digitalocean_vpc.this.id
+}
+
+output "urn" {
+  description = "The uniform resource name (URN) for the VPC"
+  value       = digitalocean_vpc.this.urn
 }
