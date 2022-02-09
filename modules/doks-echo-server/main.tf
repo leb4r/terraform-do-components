bff914061288d618e9dbfa8f67ade2a4aa8817f7
Lucky Baar <git+me@leb4r.io>
2022-02-09 17:57:03 -0600

    doks-echo-server: add options to set service annotations

--- modules/doks-echo-server/main.tf
+++ modules/doks-echo-server/main.tf
@@ -18,6 +18,16 @@ locals {
       value = "80"
     }
   ]
+
+  # see https://docs.digitalocean.com/products/kubernetes/how-to/configure-load-balancers/
+  load_balancer_service_annotations = var.load_balancer_enabled ? merge(
+    var.load_balancer_id != null ? { "kubernetes.digitalocean.com/load-balancer-id" = var.load_balancer_id } : {},
+    var.load_balancer_protocol != null ? { "service.beta.kubernetes.io/do-loadbalancer-protocol" = var.load_balancer_protocol } : {},
+    var.load_balancer_name != null ? { "service.beta.kubernetes.io/do-loadbalancer-name" = var.load_balancer_name } : {},
+    var.load_balancer_certificate_id != null ? { "service.beta.kubernetes.io/do-loadbalancer-certificate-id" = var.load_balancer_certificate_id } : {},
+    var.load_balancer_hostname != null ? { "service.beta.kubernetes.io/do-loadbalancer-hostname" = var.load_balancer_hostname } : {},
+    var.load_balancer_http2_ports != null ? { "service.beta.kubernetes.io/do-loadbalancer-http2-ports" = var.load_balancer_http2_ports } : {}
+  ) : {}
 }
 
 resource "helm_release" "echo_server" {
@@ -30,6 +40,14 @@ resource "helm_release" "echo_server" {
   atomic  = var.helm_atomic
   wait    = var.helm_wait
   timeout = var.helm_timeout
+
+  values = var.load_balancer_enabled ? compact([
+    yamlencode({
+      service = {
+        annotations = local.load_balancer_service_annotations
+      }
+    })
+  ]) : []
 
   dynamic "set" {
     for_each = { for i, v in local.load_balancer_values : i => v if var.load_balancer_enabled }
--- modules/doks-echo-server/variables.tf
+++ modules/doks-echo-server/variables.tf
@@ -39,6 +39,63 @@ variable "load_balancer_enabled" {
   default     = true
 }
 
+variable "load_balancer_id" {
+  description = "ID of the load balancer to assign to the service"
+  type        = string
+  default     = null
+}
+
+variable "load_balancer_certificate_id" {
+  description = "ID of the certificate to assign to the load balancer"
+  type        = string
+  default     = null
+}
+
+variable "load_balancer_hostname" {
+  description = ""
+  type        = string
+  default     = null
+}
+
+variable "load_balancer_http2_ports" {
+  description = <<-EOT
+    Use this annotation to specify which ports of the load balancer should use the HTTP2 protocol.
+
+    Values are a comma separated list of ports (for example, 443, 6443, 7443).
+    If specified, you must also specify either service.beta.kubernetes.io/do-loadbalancer-tls-passthrough or service.beta.kubernetes.io/do-loadbalancer-certificate-id.
+
+    If service.beta.kubernetes.io/do-loadbalancer-protocol is not set to http2, then this annotation is required for implicit HTTP2 usage.
+    Unlike service.beta.kubernetes.io/do-loadbalancer-tls-ports, no default port is assumed for HTTP2 to retain compatibility with the semantics of implicit HTTPS usage.
+  EOT
+  type        = string
+  default     = null
+}
+
+variable "load_balancer_name" {
+  description = <<-EOT
+    This setting lets you specify a custom name or to rename an existing DigitalOcean Load Balancer. The name must:
+
+    Be less than or equal to 255 characters.
+    Start with an alphanumeric character.
+    Consist of alphanumeric characters or the ‘.’ (dot) or ‘-’ (dash) characters, except for the final character which must not be a ‘-’ (dash).
+
+    If you do not specify a custom name, the load balancer defaults to a name starting with the character a appended by the Service UID.
+  EOT
+  type        = string
+  default     = null
+}
+
+variable "load_balancer_protocol" {
+  description = "The protocol of the load balancer"
+  type        = string
+  default     = null
+
+  validation {
+    condition     = contains(["tcp", "http", "https", "http2"], var.load_balancer_protocol)
+    error_message = "Error: `load_balancer_protocol` must be `tcp, http, https, or http2`."
+  }
+}
+
 variable "namespace" {
   description = "Kubernetes Namespace to deploy `echo-server` in"
   type        = string
